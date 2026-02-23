b7abbb927b44d2cb69329adf87a32c7bf811b8e7
Romain <romain.centurioni@gmail.com>
2026-02-23 11:50:05 +0100

    Add some text and img for exemple site

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <><h1>Balance ton poid</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum voluptate expedita eius neque iure fugit.</p>
+      <img src="..\..\assets\pexels-enginakyurt-1435904.jpg" alt="" />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt autem consequuntur odit labore fuga nemo consequatur perspiciatis. Cum, magni error. Sed perspiciatis hic reiciendis et autem mollitia tempore iste fugiat?</p>
+    </>)
+}
+
+export default App
